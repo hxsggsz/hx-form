fbcc531e8ac2dd2ad7a458d24a24d06aef383b07
hxsggsz <vt.hugo.2021@gmail.com>
2024-01-01 20:14:03 -0300

    add validation tests for inputs number and boolean

--- src/__tests__/useForm.spec.ts
+++ src/__tests__/useForm.spec.ts
@@ -141,7 +141,7 @@ describe("useForm", () => {
 
     describe("when has validation", () => {
       describe("the validation breaks", () => {
-        it("get errors with function validation", () => {
+        it("get errors with function validation to string", () => {
           const defaultValues = {
             test: "submit test",
           };
@@ -169,11 +169,69 @@ describe("useForm", () => {
           expect(result.current.errors).not.toBeNull();
           expect(result.current.errors?.test).toBe("error message");
         });
+
+        it("get errors with function validation to number", () => {
+          const defaultValues = {
+            test: 0,
+          };
+
+          const sumbmitEventMock = {
+            preventDefault: vi.fn(),
+          } as unknown as React.FormEvent<HTMLFormElement>;
+
+          const submitMock = vi.fn();
+
+          const { result } = renderHook(() =>
+            useForm({
+              defaultValues,
+              handleSubmit: submitMock,
+              validation: (inputs, errors) => {
+                if (inputs.test === 0) {
+                  errors.test = "error message";
+                }
+              },
+            })
+          );
+
+          act(() => result.current.onSubmit(sumbmitEventMock));
+
+          expect(result.current.errors).not.toBeNull();
+          expect(result.current.errors?.test).toBe("error message");
+        });
+
+        it("get errors with function validation to boolean", () => {
+          const defaultValues = {
+            test: false,
+          };
+
+          const sumbmitEventMock = {
+            preventDefault: vi.fn(),
+          } as unknown as React.FormEvent<HTMLFormElement>;
+
+          const submitMock = vi.fn();
+
+          const { result } = renderHook(() =>
+            useForm({
+              defaultValues,
+              handleSubmit: submitMock,
+              validation: (inputs, errors) => {
+                if (inputs.test === false) {
+                  errors.test = "error message";
+                }
+              },
+            })
+          );
+
+          act(() => result.current.onSubmit(sumbmitEventMock));
+
+          expect(result.current.errors).not.toBeNull();
+          expect(result.current.errors?.test).toBe("error message");
+        });
       });
 
       describe("zod validation breaks", () => {
         describe("custom message error", () => {
-          it("get errors with zod", () => {
+          it("get errors with zod to string validation", () => {
             const defaultValues = {
               test: "submit test",
             };
@@ -200,10 +258,38 @@ describe("useForm", () => {
             expect(result.current.errors).not.toBeNull();
             expect(result.current.errors?.test).toBe("custom message error");
           });
+
+          it("get errors with zod to number validation", () => {
+            const defaultValues = {
+              test: 0,
+            };
+
+            const sumbmitEventMock = {
+              preventDefault: vi.fn(),
+            } as unknown as React.FormEvent<HTMLFormElement>;
+
+            const submitMock = vi.fn();
+
+            const testSchema = z.object({
+              test: z.number().min(3, "custom message error"),
+            });
+            const { result } = renderHook(() =>
+              useForm({
+                defaultValues,
+                handleSubmit: submitMock,
+                schema: testSchema,
+              })
+            );
+
+            act(() => result.current.onSubmit(sumbmitEventMock));
+
+            expect(result.current.errors).not.toBeNull();
+            expect(result.current.errors?.test).toBe("custom message error");
+          });
         });
 
         describe("default message error", () => {
-          it("get errors with zod", () => {
+          it("get errors with zod string validation", () => {
             const defaultValues = {
               test: "submit test",
             };
@@ -230,6 +316,36 @@ describe("useForm", () => {
             expect(result.current.errors).not.toBeNull();
             expect(result.current.errors?.test).toBe(
               "String must contain at most 3 character(s)"
+            );
+          });
+
+          it("get errors with zod number validation", () => {
+            const defaultValues = {
+              test: 0,
+            };
+
+            const sumbmitEventMock = {
+              preventDefault: vi.fn(),
+            } as unknown as React.FormEvent<HTMLFormElement>;
+
+            const submitMock = vi.fn();
+
+            const testSchema = z.object({
+              test: z.number().min(3),
+            });
+            const { result } = renderHook(() =>
+              useForm({
+                defaultValues,
+                handleSubmit: submitMock,
+                schema: testSchema,
+              })
+            );
+
+            act(() => result.current.onSubmit(sumbmitEventMock));
+
+            expect(result.current.errors).not.toBeNull();
+            expect(result.current.errors?.test).toBe(
+              "Number must be greater than or equal to 3"
             );
           });
         });
